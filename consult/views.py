21011f902e0b50dd31b89456bc22e494f4c0e78e
Emmanuel-9 <oregadaniel181@gmail.com>
2020-09-25 15:54:34 +0300

    Add the views functions for the templates for profile,user registration, questions and add valid contact form in the contact view function.

--- consult/views.py
+++ consult/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login
+from .forms import ContactForm
 
 # Create your views here.
 def home(request):
@@ -10,22 +13,40 @@ def services(request):
     return render(request, 'services.html')
 
 def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = ContactForm()        
+
     return render(request, 'contact.html')
 
-def about(request):
+def fees(request):
     return render(request, 'fees.html')
 
 def coaching(request):
     return render(request, 'coaching.html')                
 
-def corporate(request):
-    return render(request,'corporate.html')
+def questions(request):
+    return render(request, 'questions.html')
 
-def individual(request):
-    return render(request,'individual.html')
+def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request,user)
+            return redirect('login')
+    else:
+        form = UserCreationForm()
 
-def family(request):
-    return render(request,'family.html')    
 
-def register(request):
-    return render(request, 'register.html')
+    return render(request, 'registration/registration_form.html',{"form": form})
+
+@login_required(login_url='accounts/login')
+def profile(request):
+    return render(request, 'profile.html')
