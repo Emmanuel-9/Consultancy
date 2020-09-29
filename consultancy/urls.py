24118719c9c82102a32de975c2a3f7332b0a4f2f
Emmanuel-9 <oregadaniel181@gmail.com>
2020-09-29 12:10:19 +0300

    Add the urlpatterns.

--- consultancy/urls.py
+++ consultancy/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url,include
-from django.contrib.auth import views
+from django.contrib.auth import views  
 
 urlpatterns = [
     url('admin/', admin.site.urls),
