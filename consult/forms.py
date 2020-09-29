4347a0c5d6611377e5633ca57f785980fa64b415
Emmanuel-9 <oregadaniel181@gmail.com>
2020-09-29 12:04:46 +0300

    Add the update profile and booking form.

--- consult/forms.py
+++ consult/forms.py
@@ -1,9 +1,18 @@
 from django import forms
-from phonenumber_field.formfields import PhoneNumberField
-
 
 class ContactForm(forms.Form):
     first_name = forms.CharField(label='First name',max_length=300)
     last_name = forms.CharField(label='Last name', max_length=300)
     email = forms.EmailField(label='Email address')
-    phone = PhoneNumberField()
+    
+    
+class UpdateProfileForm(forms.Form):
+    first_name = forms.CharField(label='First name',max_length=300)
+    last_name = forms.CharField(label='Last name', max_length=300)
+    email = forms.EmailField(label='Email address')
+    image = forms.ImageField()
+
+class BookingForm(forms.Form):
+    first_name = forms.CharField(label='First name',max_length=300)
+    last_name = forms.CharField(label='Last name', max_length=300)
+    
