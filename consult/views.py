ad5c6b51da68ab46964f4caa158100a61b346b1c
Emmanuel-9 <oregadaniel181@gmail.com>
2020-10-09 23:46:17 +0300

    Add the respective view functions for the templates.

--- consult/views.py
+++ consult/views.py
@@ -1,67 +1,80 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate,login
-from .forms import ContactForm,UpdateProfileForm
+from .forms import SubscriptionForm,ContactForm
+
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = SubscriptionForm() 
+    return render (request,'base.html',{"form": form})
+
+def services(request):
+    return render(request, 'services/services.html')
+
+def about(request): 
+           
+
+    return render(request, 'about/about.html')
+
+
+def case_studies(request):
+    return render(request, 'case_studies.html')                
+
+def questions(request):
+    return render(request, 'questions.html')
+
+
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def approach(request):
+    return render(request,'about/approach.html')
+
+def career(request):
+    return render(request,'about/careers.html')
+
+def purpose(request):
+    return render(request,'about/purpose.html')
+
+def recognition(request):
+    return render(request,'about/recognition.html')
+
+def team(request):
+    return render(request,'about/team.html')
+
+def building(request):
+    return render(request,'services/building.html')
+
+def change(request):
+    return render(request,'services/change.html')
+
+def conflict(request):
+    return render(request,'services/conflict.html')
+
+def digital(request):
+    return render(request,'services/digital.html')
+
+def leadership(request):
+    return render(request,'services/leadership.html')
+
+def talent(request):
+    return render(request,'services/talent.html')                    
+
+def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
             print('valid')
     else:
-        form = ContactForm() 
-    return render (request,'base.html',{"form": form})
-
-def services(request):
-    return render(request, 'services.html')
-
-def about(request):      
-    return render(request, 'about.html')
+        form = ContactForm()  
+    return render(request,'contact.html',{"form": form})
 
 
-def coaching(request):
-    return render(request, 'coaching.html')                
 
-def questions(request):
-    return render(request, 'questions.html')
-
-def registration(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request,user)
-            return redirect('home')
-    else:
-        form = UserCreationForm()
-
-
-    return render(request, 'registration/registration_form.html',{"form": form})
-
-@login_required(login_url='accounts/login')
-def profile(request):
-    return render(request, 'profile.html')
-
-def update_profile(request):
-    if request.method == 'POST':
-        form = UpdateProfileForm(request.POST)
-        if form.is_valid():
-            print('valid')
-    else:
-        form = UpdateProfileForm() 
-    return render(request,'update-profile.html',{"form": form})    
-
-def cart(request):
-    return render(request,'cart.html') 
-
-def message(request):
-    return render(request,'messages.html')        
-
-def notifications(request):
-    return render(request,'notifications.html')     
+ 
