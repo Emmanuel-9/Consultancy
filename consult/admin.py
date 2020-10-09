be0b6f0189900e32cac67ff92b36217a5190cf5e
Emmanuel-9 <oregadaniel181@gmail.com>
2020-10-09 23:20:19 +0300

    Register the ContactFormmodel.

--- consult/admin.py
+++ consult/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Client,Facilitator
+from .models import *
 
 # Register your models here.
 admin.site.register(Client)
 admin.site.register(Facilitator)
+admin.site.register(ContactForm)
