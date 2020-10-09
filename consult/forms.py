3521b1a467b792466d8d0f337779d79de2887670
Emmanuel-9 <oregadaniel181@gmail.com>
2020-10-09 23:21:41 +0300

    Add the ContactForm.

--- consult/forms.py
+++ consult/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 
-class ContactForm(forms.Form):
-    first_name = forms.CharField(label='First name',max_length=300)
-    last_name = forms.CharField(label='Last name', max_length=300)
+class SubscriptionForm(forms.Form):
     email = forms.EmailField(label='Email address')
     
     
@@ -15,4 +13,33 @@ class UpdateProfileForm(forms.Form):
 class BookingForm(forms.Form):
     first_name = forms.CharField(label='First name',max_length=300)
     last_name = forms.CharField(label='Last name', max_length=300)
-    
+
+INTEREST_CHOICES = (
+    ("Team Building","Team Building"),
+    ("Digital Marketing","Digital Marketing"),
+    ("Talent Empowerment","Talent Empowerment"),
+    ("Leadership","Leadership"),
+    ("Change Management","Change Management"),
+    ("Conflict Resolution","Conflict Resolution"),
+)  
+ABOUT_CHOICES = (
+    ("None","None"),
+    ("Referral","Referral"),
+    ("Conference","Conference"),
+    ("Networking event","Networking event"),
+    ("Search Engine(Yahoo/Google/Bing,etc)","Search Engine(Yahoo/Google/Bing,etc)"),
+    ("Email Marketing","Email Marketing"),
+    ("Others(please specify below)","Others(please specify below"),
+)      
+class ContactForm(forms.Form):
+    first_name = forms.CharField(max_length=50,label='First Name')
+    last_name = forms.CharField(max_length=50)
+    company = forms.CharField(max_length=250)
+    title = forms.CharField(max_length=250)
+    email = forms.EmailField(help_text='A valid email address, please.')
+    city = forms.CharField(max_length=50)
+    state = forms.CharField(max_length=50)
+    phone = forms.CharField(max_length=50)
+    interest =forms.ChoiceField(choices=INTEREST_CHOICES)
+    abouts = forms.ChoiceField(choices=ABOUT_CHOICES)
+    questions = forms.CharField(max_length=250,widget=forms.Textarea)
